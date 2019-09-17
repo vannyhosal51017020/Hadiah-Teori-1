cf3a0327941a55a4803d8b19dee65b9d3388f6e3
vannyhosal51017020 <48231509+vannyhosal51017020@users.noreply.github.com>
2019-09-17 21:21:35 +0800

    Add files via upload

---
+++ teori.java
@@ -0,0 +1,30 @@
+/**
+ * @(#)teori.java
+ *
+ *
+ * @author 
+ * @version 1.00 2019/9/17
+ */
+
+
+public class teori {
+	public static void main(String[] args){
+  
+int aa = 5;
+int bb = 6;
+float cc = 0.5f;
+ 
+    System.out.println("Nilai aa: "+aa);
+	System.out.println("Nilai bb: "+bb);
+	System.out.println("Nilai cc: "+cc);
+
+	
+	System.out.println("Hasil yang diperoleh");
+	double  x = aa+bb;
+	double   y = aa*bb*cc;
+	double   z = bb/cc;
+	
+	System.out.println("Nilai aa: " + x);
+	System.out.println("Nilai bb: " + y);
+	System.out.println("Nilai cc: " + z); }
+}
---
+++ teori2.java
@@ -0,0 +1,29 @@
+/**
+ * @(#)teori2.java
+ * biodata
+ *
+ * @author 
+ * @version 1.00 2019/9/17
+ */
+
+import java.util.*; 
+public class teori2 {
+	public static void main(String[] args){
+	Scanner input = new Scanner(System.in);
+    String nama,alamat,umur;
+    
+  
+      System.out.print("Nama	: "); 
+      	nama = input.nextLine();
+      System.out.print("Alamat  : "); 
+      	alamat=input.nextLine();
+      System.out.print("Umur    : "); 
+      	umur=input.nextLine();
+      	
+      	
+    
+    System.out.println("Saudara : "+ nama );
+    System.out.println("Tiggal di : "+ alamat );
+    System.out.println("Berumur :  "+ umur);
+	}  
+}
